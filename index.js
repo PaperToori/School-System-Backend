0524f0230e361372457327f933a40acf18785bbb
PaperToori <garratt.karl@elev.ga.ntig.se>
2024-03-08 14:39:07 +0100

    1st sprint: complete

--- index.js
+++ index.js
@@ -191,18 +191,16 @@ app.post('/classrooms/', async ({ body, set }) => {
     let newClassroom = new Classroom();
     newClassroom.name = body.name;
     try { await newClassroom.save(); }
-    catch (error) {
-        console.log(error.message);
-        set.status = 400;
-        return "Post: Faliure";
-    }
+    catch (error) 
+    { console.log(error.message); set.status = 400; return "Post: Faliure"; }
     return "Post: Success";
 });
 app.post('/courses/', async ({ body, set }) => {
     let newCourse = new Course();
     newCourse.name = body.name;
     try { await newCourse.save(); }
-    catch (error) { console.log(error.message); set.status = 400; return "Post: Faliure"; }
+    catch (error) 
+    { console.log(error.message); set.status = 400; return "Post: Faliure"; }
     return "Post: Success";
 });
 app.post('/groups/', async ({ body, set }) => { // Vibecheck error: Async bulshittery -> Ask Sebbe
@@ -227,6 +225,7 @@ app.post('/groups/', async ({ body, set }) => { // Vibecheck error: Async bulshi
     { console.log(error.message); set.status = 400; }
 });
 app.post('/lessons/', async ({ body, set }) => {
+    set.status = 400;
     // Input from body
     let newLesson = new Lesson();
     newLesson.classroom = body.classroom;
@@ -238,19 +237,27 @@ app.post('/lessons/', async ({ body, set }) => {
     newLesson.startTime = body.startTime;
     newLesson.endTime   = body.endTime;
     // Vibecheck
+    let check = false;
     let message = "Error:\n";
-    if (! await Classroom.exists    ({name : newLesson.classroom})) { set.status = 400; message += "Ghost classroom detected\n"; }
-    if (! await Course.exists       ({name : newLesson.course}))    { set.status = 400; message += "Ghost course detected\n"; } 
-    if (! await Group.exists        ({name : newLesson.class}))     { set.status = 400; message += "Ghost class detected\n"; }
-    if (! await Teacher.exists      ({name : newLesson.teacher}))   { set.status = 400; message += "Ghost teacher detected\n"; }
-    if (400 == set.status) { return message; }
-    if (newLesson.startTime > newLesson.endTime) { set.status = 400; return "Time travel detected."; }
+    if (! await Classroom.exists    ({name : newLesson.classroom})) 
+    { check = true;  message += "Ghost classroom detected\n"; }
+    if (! await Course.exists       ({name : newLesson.course}))    
+    { check = true; message += "Ghost course detected\n"; } 
+    if (! await Group.exists        ({name : newLesson.class}))     
+    { check = true; message += "Ghost class detected\n"; }
+    if (! await Teacher.exists      ({name : newLesson.teacher}))   
+    { check = true; message += "Ghost teacher detected\n"; }
+    if (check) { return message; }
+    if (newLesson.startTime > newLesson.endTime) 
+    { return "Time travel detected."; }
     // Save!
     try { await newLesson.save(); }
-    catch (error) { console.log(error.message); set.status = 400; return "Post: Faliure"; }
+    catch (error) { console.log(error.message); return "Post: Faliure"; }
+    set.status = 200;
     return "Post: Success";
 });
 app.post('/students/', async ({ body, set }) => {
+    set.status = 400;
     // Fill in from body
     let newStudent = new Student();
     newStudent.name         = body.name;
@@ -258,10 +265,12 @@ app.post('/students/', async ({ body, set }) => {
     newStudent.gmail        = body.gmail;
     newStudent.phoneNumber  = body.phoneNumber;
     // Vibecheck
-    if (! await Course.exists({ name: newStudent.course })) { set.status = 400; return "Course doesn't exist."; }
+    if (! await Course.exists({ name: newStudent.course })) 
+    { return "Course doesn't exist."; }
     // Save!
     try { await newStudent.save(); }
-    catch (error) { console.log(error.message); set.status = 400; return "Post: Faliure"; }
+    catch (error) { console.log(error.message); return "Post: Faliure"; }
+    set.status = 200;
     return "Post: Success";
 });
 app.post('/teachers/', async ({ body, set }) => {
@@ -282,29 +291,38 @@ app.post('/teachers/', async ({ body, set }) => {
 
 app.delete('/classrooms/', async ({ body, set }) => {
     let target = body.name;
-    if ("" == target) { set.status = 400; return "No classroom was specified." }
-    if (! await Classroom.exists({ name: target })) { set.status = 400; return "Classroom doesn't exist."; }
+    set.status = 400;
+    if ("" == target) 
+    { return "No classroom was specified." }
+    if (! await Classroom.exists({ name: target })) 
+    { return "Classroom doesn't exist."; }
     try { await Classroom.deleteOne({ name: target }); }
-    catch (error) 
-    { console.log(error); set.status = 400; return "Deletion: Faliure"; }
+    catch (error) { console.log(error); return "Deletion: Faliure"; }
+    set.status = 200;
     return "Deletion: Success";
 });
 app.delete('/courses/', async ({ body, set }) => {
     let target = body.name;
-    if ("" == target) { set.status = 400; return "No course was specified." }
-    if (! await Course.exists({ name: target })) { set.status = 400; return "Course does not exist."; }
+    set.status = 400;
+    if ("" == target) 
+    { return "No course was specified." }
+    if (! await Course.exists({ name: target })) 
+    { return "Course does not exist."; }
     try { await Course.deleteOne({ name: target }); }
-    catch (error) 
-    { console.log(error); set.status = 400; return "Deletion: Faliure"; }
+    catch (error) { console.log(error); return "Deletion: Faliure"; }
+    set.status = 200;
     return "Deletion: Success";
 })
 app.delete('/groups/', async ({ body, set }) => {
     let target = body.name;
-    if("" == target) { set.status = 400; return "No group was specified."; }
-    if (! await Group.exists({name : target})) { set.status = 400; return "Group doesn't exist."; }
+    set.status = 400;
+    if("" == target) 
+    { return "No group was specified."; }
+    if (! await Group.exists({name : target})) 
+    { return "Group doesn't exist."; }
     try { await Group.deleteOne({name : target}); } 
-    catch (error)
-    { console.log(error); set.status = 400; return "Deletion: Faliure"; }
+    catch (error) { console.log(error); return "Deletion: Faliure"; }
+    set.status = 200;
     return "Deletion: Success"; 
 });
 app.delete('/lessons/', async ({ body, set }) => { // Finish this after vibecheck overhaul
@@ -316,11 +334,15 @@ app.delete('/lessons/', async ({ body, set }) => { // Finish this after vibechec
     let targetDay       = body.weekDay;
     // Check existing
     set.status = 400;
-    if (! await Lesson.exists({ course  : targetCourse }))  { return "Course not found"; }
-    if (! await Lesson.exists({ teacher : targetTeacher })) { return "Teacher not found"; }
-    if (! await Lesson.exists({ class   : targetClass }))   { return "Class not found"; }
+    if (! await Lesson.exists({ course  : targetCourse }))  
+    { return "Course not found"; }
+    if (! await Lesson.exists({ teacher : targetTeacher })) 
+    { return "Teacher not found"; }
+    if (! await Lesson.exists({ class   : targetClass }))   
+    { return "Class not found"; }
     if (! await Lesson.exists({ week    : targetWeek, 
-                                weekDay : targetDay }))     { return "Week/Day not found"; }
+                                weekDay : targetDay }))     
+    { return "Week/Day not found"; }
     // Delete!
     try { await Lesson.deleteOne({
             teacher : targetTeacher,
@@ -334,67 +356,153 @@ app.delete('/lessons/', async ({ body, set }) => { // Finish this after vibechec
 });
 app.delete('/students/', async ({ body, set }) => {
     let target = body.name;
-    if ("" == target) { set.status = 400; return "No student was specified."; }
-    if (! await Student.exists({ name: target })) { set.status = 400; return "Student doesn't exist."; }
+    set.status = 400;
+    if ("" == target) 
+    { return "No student was specified."; }
+    if (! await Student.exists({ name: target })) 
+    { return "Student doesn't exist."; }
     try { await Student.deleteOne({ name: target }); }
-    catch (error) { console.log(error); set.status = 400; return "Deletion: Faliure"; }
+    catch (error) { console.log(error); return "Deletion: Faliure"; }
+    set.status = 200;
     return "Deletion: Success";
 });
 app.delete('/teachers/', async ({ body, set }) => {
     let target = body.name;
-    if ("" == target) { set.status = 400; return "No teacher was specified."; }
-    if (! await Teacher.exists({ name: target })) { set.status = 400; return "Teacher doesn't exist."; }
+    set.status = 400;
+    if ("" == target) 
+    { return "No teacher was specified."; }
+    if (! await Teacher.exists({ name: target })) 
+    { return "Teacher doesn't exist."; }
     try { await Teacher.deleteOne({ name: target }); }
-    catch (error) { console.log(error); set.status = 400; return "Deletion: Faliure"; }
+    catch (error) { console.log(error); return "Deletion: Faliure"; }
+    set.status = 200;
     return "Deletion: Success";
 });
 
 // ---------------------------------------------------------
 // EDIT REQUESTS -------------------------------------------
 app.patch('/classrooms/', async ({ body, set }) => { 
-    if ("" == body.target || "" == body.newName){ set.status = 400; return "Lacking input"; }
-    if (! await Classroom.exists({ name : body.target })) { set.status = 400; return "Classroom doesnt exist."; }
+    set.status = 400;
+    if ("" == body.target || "" == body.newName)
+    { return "Lacking input"; }
+    if (! await Classroom.exists({ name : body.target })) 
+    { return "Classroom doesnt exist."; }
     const target = await Classroom.findOne({name : body.target});
     target.name = body.newName;
     try { await target.save(); } 
-    catch (error) 
-    { console.log(error); set.status = 400; return "Patch: Faliure"; }
+    catch (error) { console.log(error); return "Patch: Faliure"; }
+    set.status = 200;
     return "Patch: Success"; 
  });
 app.patch('/courses/', async ({ body, set }) => { 
-    if ("" == body.target || "" == body.newName){ set.status = 400; return "Lacking input"; }
-    let check = await Course.exists({ name : body.target });
-    if (!check) { set.status = 400; return "Course doesnt exist."; }
+    set.status = 400;
+    if ("" == body.target || "" == body.newName)
+    { return "Lacking input"; }
+    if (! await Course.exists({ name : body.target })) 
+    { return "Course doesn't exist."; }
     const target = await Course.findOne({name : body.target});
     target.name = body.newName;
     try { await target.save(); } 
-    catch (error) 
-    { console.log(error); set.status = 400; return "Patch: Faliure"; }
+    catch (error) { console.log(error); return "Patch: Faliure"; }
+    set.status = 200;
     return "Patch: Success"; 
  });
-app.patch('/lessons/', async ({ body, set }) => {  });
+app.patch('/lessons/', async ({ body, set }) => { // May god have mercy on my soul
+    set.status = 400;
+    if (! await Lesson.exists({
+        classroom   : body.classroom,
+        course      : body.course,
+        teacher     : body.teacher,
+        class       : body.class,
+        week        : body.week,
+        weekDay     : body.weekDay
+    })){ return "Specified lesson doesn't exist."; }
+    const target = await Lesson.findOne({
+        classroom   : body.classroom,
+        course      : body.course,
+        teacher     : body.teacher,
+        class       : body.class,
+        week        : body.week,
+        weekDay     : body.weekDay
+    });
+    if ("" != body.newClassroom) {
+        if (! await Classroom.exists( { name : body.newClassroom })) 
+        { return "Suggested classroom doesn't exist."; }
+        target.classroom = body.newClassroom; }
+    if ("" != body.newCourse) {
+        if (! await Course.exists(    { name : body.newCourse }))    
+        { return "Suggested course doesn't exist."; }
+        target.course = body.newCourse; }
+    if ("" != body.newTeacher) {
+        if (! await Teacher.exists(   { name : body.newTeacher }))   
+        { return "Suggested teacher doesn't exist"; }
+        target.teacher = body.newTeacher; }
+    if ("" != body.newClass) {
+        if (! await Group.exists(     { name : body.newClass }))     
+        { return "Suggested class doesn't exist"; }
+        target.class = body.newClass; }
+    if (-1 != body.newWeek) {
+        if (1 > body.newWeek || 52 < body.newWeek)                 
+        { return "Suggested week is outside Gregorian standard."; }
+        target.class = body.newClass; }
+    if (-1 != body.newWeekDay) {
+        if (1 > body.newWeekDay || 5 < body.newWeekDay)         
+        { return "Suggested weekday is outside Gregorian standard."; }
+        target.weekDay = body.newWeekDay; }
+    if (-1 != body.newStartTime) {
+        if (0 > body.newStartTime || 143 < body.newStartTime) 
+        { return "Illegal start time."; }
+        target.startTime = body.newStartTime; }
+    if (-1 != body.newEndTime) {
+        if (target.startTime > body.newEndTime)
+        { return "Time travel detected."; }
+        if (143 < body.newEndTime)
+        { return "Illegal end time."; }
+        target.endTime = body.newEndTime; }
+    try { await target.save(); }
+    catch (error){ console.log(error); return "Patch: Faliure"; }
+    set.status = 200; 
+    return "Patch: Success"; 
+ });
 app.patch('/groups/', async ({ body, set }) => {  }); // Don't even bother
-app.patch('/students/', async ({ body, set }) => { // This isn't done yet ... bug: text fields are emptied upon if-checking
-    if ("" == body.target){ set.status = 400; return "Lacking target"; }
-    if (! await Student.exists({ name : body.target })) { set.status = 400; return "Student doesnt exist."; }
-    const target = await Student.findOne({name : body.target});
-    console.log(target);
+app.patch('/students/', async ({ body, set }) => {
+    set.status = 400;
+    if ("" == body.target){ return "Lacking target"; }
+    if (! await Student.exists({ name : body.target })) 
+    { return "Student doesnt exist."; }
+    const target = await Student.findOne({ name : body.target });
     if ("" != body.newName) {
-        if (await Student.exists({ name: body.newName}))
-        { set.status = 400; return "Student with suggested name already exists."; }
-        else { target.name = body.newName; }  
-    }
-    console.log(target);
-    if ("" != body.course) { target.course = body.newCourse }
+        if (await Student.exists({ name: body.newName })) 
+        { return "Student with suggested name already exists."; }
+        target.name = body.newName; }
+    if ("" != body.newCourse) { 
+        if (! await Course.exists({ name : body.newCourse })) 
+        { return "Suggested course doesn't exist."; }
+        target.course = body.newCourse; }
     if ("" != body.newGmail) { target.gmail = body.newGmail; }
     if ("" != body.newPhoneNumber) { target.phoneNumber = body.newPhoneNumber; }
-    console.log(target);
     try { await target.save(); } 
-    catch (error) 
-    { console.log(error); set.status = 400; return "Patch: Faliure"; }
+    catch (error) { console.log(error); return "Patch: Faliure"; }
+    set.status = 200;
     return "Patch: Success"; 
  });
-app.patch('/teachers/', async ({ body, set }) => {  });
+app.patch('/teachers/', async ({ body, set }) => {
+    set.status = 400;
+    if ("" == body.target){ return "Lacking target"; }
+    if (! await Teacher.exists({ name : body.target })) 
+    { return "Teacher doesnt exist."; }
+    const target = await Teacher.findOne({ name : body.target });
+    if ("" != body.newName) {
+        if (await Teacher.exists({ name: body.newName })) 
+        { return "Teacher with suggested name already exists."; }
+        target.name = body.newName; }
+    if ("" != body.newGmail) { target.gmail = body.newGmail; }
+    if ("" != body.newPhoneNumber) { target.phoneNumber = body.newPhoneNumber; }
+    try { await target.save(); } 
+    catch (error) { console.log(error); return "Patch: Faliure"; }
+    set.status = 200;
+    return "Patch: Success"; 
+ });
 
 
 // ---------------------------------------------------------
