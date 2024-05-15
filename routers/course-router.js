0d6303a0f779f565894b3023dfa7238d83068718
0n10n7 <mikael.kokeritz@gmail.com>
2024-05-15 11:10:18 +0200

    courses post and courses delete work

--- routers/course-router.js
+++ routers/course-router.js
@@ -11,11 +11,26 @@ export const course_router = new Elysia({ prefix: '/course' })
         {
             async beforeHandle({ set, headers }) {
                 let id = headers.id;
+                if (await Authenticator(2, id) == false) {
+                    console.log("NOT APPROVED!");
+                    return (set.status = 'Unauthorized');
+                }
+            }
+        },
+        (App) =>
+            App
+                .get("/", async () => {
+                    return await Course.find();
+                })
+    )
+    .guard(
+        {
+            async beforeHandle({ set, headers }) {
+                let id = headers.id;
                 if (await Authenticator(3, id) == false) {
                     console.log("NOT APPROVED!");
                     return (set.status = 'Unauthorized');
                 }
-                console.log("APPROVED");
             }
         },
         (App) =>
@@ -25,38 +40,66 @@ export const course_router = new Elysia({ prefix: '/course' })
                     let parsedBody = JSON.parse(body);
                     let course = new Course();
                     course.name = parsedBody.name;
-                    const subject = await Subject.findOne({ name: parsedBody.subject })
+                    const subject = await Subject.findOne({ name: parsedBody.subject });
                     if (subject != null) {
-                        course.subject = subject.id;
+                        course.subject = subject.name;
                     }
-                    else{
+                    else {
                         set.status = "Bad Request";
                         return "subject doesn't exsist";
                     }
-                    let teacher = await Teacher.findOne({ socialSecurityNumber: parsedBody.teacher })
+                    const group = await Group.findOne({ name: parsedBody.group });
+                    if (group != null) {
+                        course.group = group.name;
+                    }
+                    else {
+                        set.status = "Bad Request";
+                        return "Course doesn't exsist";
+                    }
+                    let teacher = await Teacher.findOne({ _id: parsedBody.teacher });
                     if (teacher != null) {
                         course.teacher = teacher.id;
                         teacher.course.push(course.name);
                         try {
                             await teacher.save();
                         } catch (error) {
-                            console.log("teacher save: ",error.message);
+                            console.log("teacher save: ", error.message);
                             return "Post: Faliure";
                         }
                     }
-                    else{
+                    else {
                         set.status = "Bad Request";
                         return "teacher doesn't exsist";
                     }
                     // Save!
                     try {
+                        console.log(`saving course: ${course}`);
                         await course.save();
                         set.status = 200;
                         return "Post: Success";
                     } catch (error) {
-                        console.log("course save: ",error.message);
+                        console.log("course save: ", error.message);
                         return "Post: Faliure";
                     }
+                })
+                .delete("/", async ({ set, body }) => {
+                    let target = JSON.parse(body).name;
+                    set.status = 500;
+                    if (null == target) {
+                        return "No course was specified.";
+                    }
+                    console.log(target);
+                    if (!(await Course.exists({ name: target }))) {
+                        return "Course doesn't exist.";
+                    }
+                    try {
+                        await Course.deleteOne({ name: target });
+                    } catch (error) {
+                        console.log(error);
+                        return "Deletion: Faliure";
+                    }
+                    set.status = 200;
+                    return "Deletion: Success";
                 })
     )
     ;
